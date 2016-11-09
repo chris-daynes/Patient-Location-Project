8d54b0b2af0e8fc7fa2d121f49054b232d94ae9d
chris-daynes <chrisdaynes@gmail.com>
2016-11-09 13:02:08 +1300

    added getPatientLocation function, not working yet

--- db/patients.js
+++ db/patients.js
@@ -3,9 +3,16 @@ var knexConfig = require('../knexfile')[process.env.NODE_ENV || 'development']
 var knex = Knex(knexConfig)
 
 module.exports = {
-  getPts
+  getPts,
+  getPatientLocation
 }
 
 function getPts () {
   return knex('patients')
+}
+
+function getPatientLocation (patientId) {
+  return knex.from('patients')
+    .innerJoin('locations', 'patient.location_id', '=', 'location.id')
+    .where('patient.id', '=', patientId)
 }
