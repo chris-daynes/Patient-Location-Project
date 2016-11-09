c3d1576d38b0157562de01f50b815b724d021cc4
chris-daynes <chrisdaynes@gmail.com>
2016-11-09 16:32:20 +1300

    working patientList function but still not integrating with other two tables

--- db/functions.js
+++ db/functions.js
@@ -4,15 +4,30 @@ var knex = Knex(knexConfig)
 
 module.exports = {
   getPts,
-  getPatientLocation
+  getPatientsLocation,
+  getPatientsConsultant,
+  logError,
+  closeDB
 }
 
 function getPts () {
   return knex('patients')
 }
 
-function getPatientLocation (patientId) {
-  return knex.from('patients')
-    .innerJoin('locations', 'patients.location_id', '=', 'locations.id')
-    .where('patients.id', '=', patientId)
+function getPatientsLocation() {
+  return knex.select('locations.location').from('patients')
+    .join('locations', 'patients.location_id', '=', 'locations.id')
+}
+
+function getPatientsConsultant () {
+  return knex.select('consultants.name').from('patients')
+    .join('consultants', 'patients.consultant_id', '=', 'consultants.id')
+}
+
+function logError (err) {
+  console.log('Man we Blew up!:', err)
+}
+
+function closeDB () {
+  knex.destroy()
 }
--- routes/patients.js
+++ routes/patients.js
@@ -1,16 +1,14 @@
 var express = require('express')
 var router = express.Router()
-var getPatientList = require('../db/functions.js').getPts
-var getPatientLocation = require('../db/functions.js').getPatientLocation
+var getPatientList = require('../db/functions').getPts
+var getPatientsLocation = require('../db/functions').getPatientsLocation
+var getPatientsConsultant = require('../db/functions').getPatientsConsultant
 
 // gets a list of all the pts
 router.get('/', function (req, res) {
-  getPatientLocation(1)
-    .then(data => console.log(data))
-
-
   getPatientList()
     .then(function (ptsFromDB) {
+      console.log(ptsFromDB)
       res.render('patients', { patients: ptsFromDB })
     })
     .catch(function (error) {
